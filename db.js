dcbc50023eab0a3e0fa4db7ad10570bb8cd5fdba
Lam Phan <lam2103@tamu.edu>
2026-04-07 03:30:26 -0500

    fixing the backend because it not getting the order it giving errors

--- db.js
+++ db.js
@@ -1,19 +1,15 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const useSSL = process.env.DB_SSLMODE === "require";
-
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  ssl: useSSL
-    ? {
-        rejectUnauthorized: false
-      }
-    : false
+  host: process.env.PSQL_HOST,
+  port: Number(process.env.PSQL_PORT),
+  database: process.env.PSQL_DATABASE,
+  user: process.env.PSQL_USER,
+  password: process.env.PSQL_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
