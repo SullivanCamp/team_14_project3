bcc3379df07c86c85763fea72702d6637f75a0e4
Dylan Devasia <dylandevasia@tamu.edu>
2026-04-08 23:32:02 -0500

    added search bar to inventory

--- public/js/inventoryManagement.js
+++ public/js/inventoryManagement.js
@@ -1,3 +1,27 @@
+// From W3 Schools
+function SearchFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("Search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("inventory-table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.visibility = "visible"; 
+      } else {
+        tr[i].style.visibility = "collapse"; // hides row but keeps table layout
+      }
+    }
+  }
+}
+
+
 function openEditForm(button) {
     const editForm = document.querySelector('.edit');
     editForm.style.display = 'block';
